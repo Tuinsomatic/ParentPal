216df70b9e037e9799d4bac04ed618a52b3f85af
Malleoluses R <33025211+Tuinsomatic@users.noreply.github.com>
2019-03-27 17:09:32 +0000

    Further code plus button bug
    Ross, I need a hand!

--- ParentPal - PC Prototype/Assets/Scripts/SceneChanger.cs
+++ ParentPal - PC Prototype/Assets/Scripts/SceneChanger.cs
@@ -6,20 +6,21 @@ using UnityEngine.UI;
 public class SceneChanger : MonoBehaviour {
 
     public SceneDatabase scenes = new SceneDatabase();
+    public Scene currentScene;
 
-	// Use this for initialization
-	void Start () {
-        ChangeScene(scenes.scene1);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    void Start () {
+        currentScene = scenes.scene1;
+        ChangeScene(); //Scenario 1 is the first one to be loaded (for now)
 	}
 
-    void ChangeScene(Scene scene) //the fn used to change scenes when a button is pressed
+    public void ChangeScene() //the fn used to change scenes when a button is pressed
     {
-        GameObject q = GameObject.Find("Question");
-        q.GetComponent<Text>().text = scene.question;
+        GameObject q = GameObject.Find("Question"); //Question is the Text UI element at the top of the screen
+        GameObject o1 = GameObject.Find("Option1Text");
+        GameObject o2 = GameObject.Find("Option2Text");
+
+        q.GetComponent<Text>().text = currentScene.question; //Replace the text on it with the question text from scene
+        o1.GetComponent<Text>().text = currentScene.option1.text;
+        o2.GetComponent<Text>().text = currentScene.option2.text;
     }
 }
