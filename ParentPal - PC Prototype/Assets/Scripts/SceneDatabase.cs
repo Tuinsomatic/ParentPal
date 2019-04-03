ede27a3e5ce4b2d3a652be2053ec8ae1aa692d29
Malleoluses R <33025211+Tuinsomatic@users.noreply.github.com>
2019-04-03 16:04:14 +0100

    Database and SceneChanger join
    combined code from SceneChanger and Scene Database
    
    Buttons now work
    
    Omitted the Option class, and stored option texts in one list, and option destinations in another list
    ~this allows for less code redundancy
    
    **Fixed the issue I addressed in the last commit

--- ParentPal - PC Prototype/Assets/Scripts/SceneDatabase.cs
+++ ParentPal - PC Prototype/Assets/Scripts/SceneDatabase.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class SceneDatabase : MonoBehaviour {
@@ -13,67 +14,41 @@ public class SceneDatabase : MonoBehaviour {
     private void Start()
     {
         scene1.question = "Teen sat playing on games console in messy bedroom. Carer knocks on the door and walks in…";
-        scene1.option1 = new Option("Carer flies off the handle… We’ve got to go out soon – get off that blasted computer! And look at the state of this room!When is the last time you cleaned it?", scene2);
-        scene1.option2 = new Option("Carer remains calm: Insert teen name, we’re leaving in half an hour.Can you finish playing your game, get changed and be ready in the kitchen by 7 o’clock.I will give you a reminder in 15 minutes. If you are still on the console, I will turn it off.", scene3);
-        scene1.option3 = null;
-        scene1.option4 = null;
-        scene1.option5 = null;
+        scene1.optionT.Add("Get ma");
+        scene1.optionT.Add("Remain clm");
+        scene1.optionD.Add(scene2);
+        scene1.optionD.Add(scene3);
+
 
         scene2.question = "Teen shouts back at carer";
-        scene2.option1 = new Option("Storm out", scene4);
-        scene2.option2 = new Option("Hit teen", scene2);
-        scene2.option3 = null;
-        scene2.option4 = null;
-        scene2.option5 = null;
+
 
         scene3.question = "Teen replies, ok carer no worries";
-        scene3.option1 = null;
-        scene3.option2 = null;
-        scene3.option3 = null;
-        scene3.option4 = null;
-        scene3.option5 = null;
+
 
         scene4.question = "You storm out";
-        scene4.option1 = null;
-        scene4.option2 = null;
-        scene4.option3 = null;
-        scene4.option4 = null;
-        scene4.option5 = null;
 
 
+        ChangeScene(scene1);
         //scenes[0] = new Scene();
+    }
+
+    public void ChangeScene(Scene scene) //the fn used to change scenes when a button is pressed
+    {
+        GameObject q = GameObject.Find("Question"); //Question is the Text UI element at the top of the screen
+        q.GetComponent<Text>().text = scene.question; //Replace the text on it with the question text from scene
+
+        for (int i = 0; i<=scene.optionT.Count; i++)
+        {
+            GameObject button = GameObject.Find("Option" + (i+1).ToString() + "Text");
+            button.GetComponent<Text>().text = scene.optionT[i];
+        }
     }
 }
 
 public class Scene
 {
     public string question;
-    public Option option1;
-    public Option option2;
-    public Option option3;
-    public Option option4;
-    public Option option5;
-    //public Sprite background = null;
-
-    //public Scene(string q, Option o1, Option o2, Option o3, Option o4, Option o5)
-    //{
-    //    q = question;
-    //    o1 = option1;
-    //    o2 = option2;
-    //    o3 = option3;
-    //    o4 = option4;
-    //    o5 = option5;
-    //}
-}
-
-public class Option
-{
-    public string text;
-    public Scene destination;
-
-    public Option(string t, Scene d)
-    {
-        t = text;
-        d = destination;
-    }
+    public List<string> optionT = new List<string>();
+    public List<Scene> optionD = new List<Scene>();
 }
