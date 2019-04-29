1e10140fd7693218c7a18ab7b25ed7a0a3d11615
Malleoluses R <33025211+Tuinsomatic@users.noreply.github.com>
2019-04-29 16:56:53 +0100

    Scenario 1 implemented
    Scenario 1 implemented

--- ParentPal - PC Prototype/Assets/Scripts/SceneDatabase.cs
+++ ParentPal - PC Prototype/Assets/Scripts/SceneDatabase.cs
@@ -10,6 +10,7 @@ public class SceneDatabase : MonoBehaviour {
     public Scene scene2 = new Scene();
     public Scene scene3 = new Scene();
     public Scene scene4 = new Scene();
+    public Scene scene5 = new Scene();
 
     public Scene current;
 
@@ -19,26 +20,36 @@ public class SceneDatabase : MonoBehaviour {
     private void Start()
     {
         scene1.question = "Teen sat playing on games console in messy bedroom. Carer knocks on the door and walks in…";
-        scene1.optionT.Add("Get ma");
-        scene1.optionT.Add("Remain clm");
+        scene1.optionT.Add("Get MAD & comment on state of bedroom");
+        scene1.optionT.Add("Remain calm & ask teen to be ready to go out in the kitchen soon");
         scene1.optionD.Add(scene2);
-        scene1.optionD.Add(scene3);
+        scene1.optionD.Add(scene4);
 
 
         scene2.question = "Teen shouts back at carer";
-        scene2.optionT.Add("endgame");
-        scene2.optionT.Add("spoilers");
-        scene2.optionD.Add(scene1);
-        scene2.optionD.Add(scene1);
+        scene2.optionT.Add("Remain angry");
+        scene2.optionT.Add("Back down, but give a warning that the console is coming off if the teen isn't ready soon");
+        scene2.optionD.Add(scene3);
+        scene2.optionD.Add(scene4);
 
-        scene3.question = "Teen replies, ok carer no worries";
-        scene3.optionT.Add("eat shit");
-        scene3.optionT.Add("i hate unity");
+        scene3.question = "Both you and teen are angry and upset. Teen shuts down, refuses to go out. Evening ruined";
+        scene3.optionT.Add("Go back");
+        scene3.optionT.Add("Go back");
         scene3.optionD.Add(scene1);
         scene3.optionD.Add(scene1);
 
 
-        scene4.question = "You storm out";
+        scene4.question = "Teen replies: \"no worries\". 15 mins later, carer comes back in to remind teen to be ready soon.";
+        scene4.optionT.Add("Turn console off and give 15 minute warning");
+        scene4.optionT.Add("Give 15 minute warning");
+        scene4.optionD.Add(scene3);
+        scene4.optionD.Add(scene5);
+
+        scene5.question = "Teen is ready and downstairs on time. Evening out goes forward as planned.";
+        scene5.optionT.Add("Scenario over");
+        scene5.optionT.Add("Scenario over");
+        scene5.optionD.Add(scene5);
+        scene5.optionD.Add(scene5);
 
         current = scene1;
         //for (int i = 0; i <= current.optionT.Count; i++)
@@ -66,11 +77,6 @@ public class SceneDatabase : MonoBehaviour {
         }
 
         current = scene;
-    }
-//---------------------------------------------------------------------------------------------------------------------------------------
-    private void Update()
-    {
-
     }
 }
 //---------------------------------------------------------------------------------------------------------------------------------------
