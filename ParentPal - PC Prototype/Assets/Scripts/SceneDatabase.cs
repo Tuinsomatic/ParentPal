19a8ba67b2b5c05d27dea8a38997edcd986b62c3
Martin McKenna <33032026+MartinMcK@users.noreply.github.com>
2019-05-02 14:27:13 +0100

    Main Menu Added

---
+++ ParentPal - PC Prototype/Assets/Scripts/SceneDatabase.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SceneDatabase : MonoBehaviour {
+
+    //public List<Scene> scenes = new List<Scene>();
+    public Scene scene1 = new Scene();
+    public Scene scene2 = new Scene();
+    public Scene scene3 = new Scene();
+    public Scene scene4 = new Scene();
+    public Scene scene5 = new Scene();
+
+    public Scene current;
+
+    public Button op1;
+    public Button op2;
+//---------------------------------------------------------------------------------------------------------------------------------------
+    private void Start()
+    {
+        scene1.question = "Teen sat playing on games console in messy bedroom. Carer knocks on the door and walks in…";
+        scene1.optionT.Add("Get MAD & comment on state of bedroom");
+        scene1.optionT.Add("Remain calm & ask teen to be ready to go out in the kitchen soon");
+        scene1.optionD.Add(scene2);
+        scene1.optionD.Add(scene4);
+
+
+        scene2.question = "Teen shouts back at carer";
+        scene2.optionT.Add("Remain angry");
+        scene2.optionT.Add("Back down, but give a warning that the console is coming off if the teen isn't ready soon");
+        scene2.optionD.Add(scene3);
+        scene2.optionD.Add(scene4);
+
+        scene3.question = "Both you and teen are angry and upset. Teen shuts down, refuses to go out. Evening ruined";
+        scene3.isEnd = true;
+
+
+        scene4.question = "Teen replies: \"no worries\". 15 mins later, carer comes back in to remind teen to be ready soon.";
+        scene4.optionT.Add("Turn console off and give 15 minute warning");
+        scene4.optionT.Add("Give 15 minute warning");
+        scene4.optionD.Add(scene3);
+        scene4.optionD.Add(scene5);
+
+        scene5.question = "Teen is ready and downstairs on time. Evening out goes forward as planned.";
+        scene5.isEnd = true;
+
+        current = scene1;
+        //for (int i = 0; i <= current.optionT.Count; i++)
+        //{
+        //    GameObject button = GameObject.Find("Option" + (i + 1).ToString());
+        //    button.onClick.AddListener
+        //}
+        
+        op1.onClick.AddListener(() => ChangeScene(GameObject.Find("Option1Text").GetComponent<Destination>().nextDest));
+        op2.onClick.AddListener(() => ChangeScene(GameObject.Find("Option2Text").GetComponent<Destination>().nextDest));
+
+        ChangeScene(current);
+    }
+//---------------------------------------------------------------------------------------------------------------------------------------
+    public void ChangeScene(Scene scene) //the fn used to change scenes when a button is pressed
+    {
+        GameObject q = GameObject.Find("Question"); //Question is the Text UI element at the top of the screen
+        q.GetComponent<Text>().text = scene.question; //Replace the text on it with the question text from scene
+
+        if (scene.isEnd == false)
+        {
+            for (int i = 0; i <= scene.optionT.Count; i++)
+            {
+                GameObject buttonText = GameObject.Find("Option" + (i + 1).ToString() + "Text");
+                buttonText.GetComponent<Text>().text = scene.optionT[i];
+                buttonText.GetComponent<Destination>().nextDest = scene.optionD[i];
+            }
+        }
+        else
+        {
+            for (int i = 0; i <= 2; i++)
+            {
+                GameObject button = GameObject.Find("Option" + (i + 1).ToString());
+                button.SetActive(false);
+            }
+        }
+
+        current = scene;
+    }
+}
+//---------------------------------------------------------------------------------------------------------------------------------------
+public class Scene
+{
+    public string question;
+    public List<string> optionT = new List<string>();
+    public List<Scene> optionD = new List<Scene>();
+    public bool isEnd = false;
+}
