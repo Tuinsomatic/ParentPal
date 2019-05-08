21365d08c8b2a4212d7b6314cda21c3b7159c7a9
rdoran1 <33032112+rdoran1@users.noreply.github.com>
2019-05-08 22:13:27 +0100

    Continuation of Character Creation

--- ParentPal - PC Prototype/Assets/Scripts/CharacterCreation.cs
+++ ParentPal - PC Prototype/Assets/Scripts/CharacterCreation.cs
@@ -1,45 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterCreation : MonoBehaviour
 {
     // Start is called before the first frame update
-    GameObject button;
-    GameObject topOption;
-    GameObject bottomOption;
-    GameObject hairOption;
-    GameObject hairColour;
-    GameObject skinColour;
+    Button button;
+
+    public GameObject maleTopOption1;
+    public GameObject maleTopOption2;
+    public GameObject maleTopOption3;
+
+    public GameObject maleBottomOption1;
+    public GameObject maleBottomOption2;
+    public GameObject maleBottomOption3;
+
+    //public GameObject hairOption;
+    //public GameObject hairColour;
+    //public GameObject skinColour;
 
     void Start()
     {
+        maleTopOption1.SetActive(false);
+        maleTopOption2.SetActive(false);
+        maleTopOption3.SetActive(false);
 
-        
+        maleBottomOption1.SetActive(false);
+        maleBottomOption2.SetActive(false);
+        maleBottomOption3.SetActive(false);
 
     }
 
-
-    private void OnMouseUpAsButton()
+    void ButtonPress()
     {
-        if (button = GameObject.Find("TopButton"))
-        {
-            int i = 0;
-            i++;
-
-            if (i == 0)
-            {
-
-            }
-            else if (i == 1)
-            {
-
-            }
-            else if (i == 2)
-            {
-
-            }
-        }
+        button.onClick.AddListener(() => ch);
     }
 
+    void CharacterEdit()
+    {
+
+    }
 }
+
